f15584260451423e1737f959bd754bc393931ae8
amadorcf <amadorcf@outlook.com>
2024-06-07 22:42:40 +0200

    3d effect myImg

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -10,15 +10,44 @@ export class AboutComponent {
   public subtitle: string | undefined;
   public email: string | undefined;
 
-  constructor(){
+  @ViewChild('cardContainer') cardContainer!: ElementRef;
+
+  constructor(private renderer: Renderer2) {
     this.title = "Amador Cano Fernández";
     this.subtitle = "Future Full-Stack Developer";
     this.email = "amadorcf@outlook.com"
+   }
+
+   ngAfterViewInit(): void {
+    // Asegúrate de que el DOM esté completamente cargado
+    this.renderer.listen(this.cardContainer.nativeElement, 'mousemove', (e: MouseEvent) => {
+      this.onMouseMove(e);
+    });
+    this.renderer.listen(this.cardContainer.nativeElement, 'mouseout', () => {
+      this.onMouseOut();
+    });
+
+
   }
 
-  ngOnInit(): void {
+  onMouseMove(e: MouseEvent): void {
+    const card = this.cardContainer.nativeElement;
+    const cardRect = card.getBoundingClientRect();
 
+    const xPosition = (e.clientX - cardRect.left) / cardRect.width;
+    const yPosition = 0; //(e.clientY - cardRect.top) / cardRect.height - 0.6;
+    const xOffset = -(xPosition - 0.6);
+    const dxNorm = Math.min(Math.max(xOffset, -0.6), 0.6);
 
+    this.renderer.setStyle(card, 'transform', `perspective(1000px)
+      rotateY(${dxNorm * 75}deg)
+      rotateX(${yPosition * 75}deg)`);
   }
+
+  onMouseOut(): void {
+    const card = this.cardContainer.nativeElement;
+    this.renderer.setStyle(card, 'transform', 'none');
+  }
+
 
 }
